d1cdabf3c1f648f4583ecca90ba056b76756078d
toshi1014 <toshihide1014go@gmail.com>
2021-08-15 14:09:30 +0900

    fix: submodule path

--- src/AppContent.tsx
+++ src/AppContent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 import Header from "./components/Header";
-import GenerateMessage from "./backend/generateMessage";
+import GenerateMessage from "./UnLINE-backend/generateMessage";
 
 
 function AppContent() {
