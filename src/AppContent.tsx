e4556ba56be92f7be05208c3ef6b99a3edf9e6cc
Yuta Uchijo <kgtnttki.yuta@gmail.com>
2021-08-20 23:26:23 +0900

    add: send/receive button

--- src/AppContent.tsx
+++ src/AppContent.tsx
@@ -1,32 +1,55 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, ScrollView, FlatList } from 'react-native';
 import Header from "./components/Header";
 import GenerateMessage from "./UnLINE-backend/generateMessage";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NativeBaseProvider, Box } from 'native-base';
+import { NativeBaseProvider, Box, Button, Center, Avatar } from 'native-base';
 
 
 function HomeScreen({navigation}) {
-    const [name, setName] = React.useState("John Smith");
-
-    const generateMessage = new GenerateMessage();
-    const message = generateMessage.getGeneratedMessage(name);
 
     return (
-        <View>
-            <Header name={name}/>
-            <Text>Hello, World!</Text>
-            <Text>message: {message}</Text>
-            <Button title='talk with aaaa' onPress={() => navigation.navigate('Talk')} />
-        </View>
+        <NativeBaseProvider>
+            <Box>
+                <Button onPress={() => navigation.navigate('Talk')}>Talk</Button>
+            </Box>
+        </NativeBaseProvider>
     )
 }
 
 function TalkScreen() {
+    const [messageList, setMessageList] = useState([]);
+
+    const handleSend = () => {
+        let newMessageList = messageList.slice();
+        let newMessage = {key: 'send: message' + messageList.length};
+        newMessageList.push(newMessage);
+        setMessageList(newMessageList);
+    }
+
+    const handleReceive = () => {
+        let newMessageList = messageList.slice();
+        let newMessage = {key: 'receive: message' + messageList.length};
+        newMessageList.push(newMessage);
+        setMessageList(newMessageList);
+    }
+
     return (
         <NativeBaseProvider>
-            <Box>Hello, world</Box>
+            <Box style={{flex:1}}>
+                <FlatList
+                    data={messageList}
+                    renderItem={({item}) => <Text>{item.key}</Text>}
+                />
+
+                <Center>
+                    <Button.Group>
+                        <Button onPress={handleReceive}>receive</Button>
+                        <Button onPress={handleSend}>send</Button>
+                    </Button.Group>
+                </Center>
+            </Box>
         </NativeBaseProvider>
     );
 }
