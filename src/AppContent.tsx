956ab3303c35f1f686b751e27ba9e6cb78880468
Yuta Uchijo <kgtnttki.yuta@gmail.com>
2021-09-04 22:50:41 +0900

    change: misc

--- src/AppContent.tsx
+++ src/AppContent.tsx
@@ -1,27 +1,18 @@
 import React, { useState } from 'react';
 import { View, Text, ScrollView, FlatList } from 'react-native';
-import Header from "./components/Header";
-import GenerateMessage from "./UnLINE-backend/generateMessage";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeBaseProvider, Box, Button, Center, Input } from 'native-base';
 import TalkScreen from './TalkScreen';
-import DocumentPicker from 'react-native-document-picker';
 
 
 function HomeScreen({ navigation }) {
-    let fileInfo;
     const [content, setContent] = useState('');
 
     return (
         <NativeBaseProvider>
             <Box>
-                <Button onPress={() => navigation.navigate('Talk', { name: fileInfo.name, uri: fileInfo.uri, content: content })}>import talk history and Talk</Button>
-                <Button onPress={() => {
-                    pickOneFile().then((res) => {
-                        fileInfo = res;
-                    });
-                }}>pick file</Button>
+                <Button onPress={() => navigation.navigate('Talk', { content: content })}>import talk history and Talk</Button>
                 <Input
                     onChangeText={text => setContent(text)}
                     defaultValue={content}
@@ -30,24 +21,6 @@ function HomeScreen({ navigation }) {
             </Box>
         </NativeBaseProvider>
     )
-}
-
-const pickOneFile = async () => {
-    try {
-        const res = await DocumentPicker.pick({
-            type: [DocumentPicker.types.allFiles],
-        });
-        console.log(res[0].name);
-        console.log(res[0].uri);
-        return res[0];
-    } catch (err) {
-        if (DocumentPicker.isCancel(err)) {
-            console.log("canceled");
-            return 'picking canceled';
-        } else {
-            throw err;
-        }
-    }
 }
 
 const Stack = createNativeStackNavigator();
--- src/TalkScreen.tsx
+++ src/TalkScreen.tsx
@@ -1,28 +1,19 @@
 import React, { useState } from 'react';
 import { View, Text, ScrollView, FlatList } from 'react-native';
 import GenerateMessage from "./UnLINE-backend/GenerateMessage";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeBaseProvider, Box, Button, Center, Avatar } from 'native-base';
-import RNFS from 'react-native-fs';
 
 export default function TalkScreen({route}) {
     const [messageList, setMessageList] = useState([]);
-    const {name, uri, content} = route.params;
-
-    const fileuri = JSON.stringify(uri);
+    const {content} = route.params;
 
     const generateMessage = new GenerateMessage(JSON.stringify(content))
-    
-    //getRealPath(JSON.stringify(uri)).then((result) => {console.log(result)});
-    //const generateMessage = new GenerateMessage(content);
-    //console.log("message: ", generateMessage.getGeneratedMessage("生方"));
-
     const handleMessageButton = (isSender: boolean) => {
         let newMessageList = messageList.slice();
         let newMessage = {
             key: messageList.length,
-            content: isSender ? generateMessage.getGeneratedMessage("sender") : generateMessage.getGeneratedMessage(generateMessage.opponentName),
+            content: 'aaa',
+            //content: isSender ? generateMessage.getGeneratedMessage("sender") : generateMessage.getGeneratedMessage(generateMessage.opponentName),
             isSender: isSender
         };
         newMessageList.push(newMessage);
@@ -32,8 +23,6 @@ export default function TalkScreen({route}) {
     return (
         <NativeBaseProvider>
             <Box style={{flex:1}}>
-                <Box>{JSON.stringify(uri)}</Box>
-                <Box>{JSON.stringify(name)}</Box>
                 <FlatList
                     data={messageList}
                     renderItem={({item}) => <Text>{item.content}</Text>}
